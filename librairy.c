1f3a2a287eef9bedf56c53a7f45c100aee168bbf
fresti <33902893+fresti@users.noreply.github.com>
2018-05-17 10:04:04 +0200

    librairy.c

---
+++ librairy.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "librairy.h"
+
+int multiplication(int a, int b){
+  int c;
+  
+  c = a * b;
+  
+  return c;
+}  
+  
+int division(int a, int b){
+  int c;
+  
+  c = a / b;
+  
+  return c;
+}  
+  
+int addition(int a, int b){
+  int c;
+  
+  c = a + b;
+  
+  return c;
+}
